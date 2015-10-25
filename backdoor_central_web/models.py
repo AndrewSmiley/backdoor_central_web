9d5f60893e9ca93f01d4f5fb3ccd85487739f98d
Andrew Smiley <andrew.smiley@live.com>
2015-10-25 15:54:55 -0400

    logins working

--- backdoor_central_web/models.py
+++ backdoor_central_web/models.py
@@ -2,8 +2,9 @@ __author__ = 'pridemai'
 from django.db import models
 
 class VirtualMachine(models.Model):
-    filename=models.CharField(max_length=70)
-    display_name=models.CharField(max_length=50)
-
+    filename=models.CharField(max_length=70,null=False)
+    display_name=models.CharField(max_length=50,null=False)
+    username = models.CharField(max_length=50, null=False)
+    password = models.CharField(max_length=50, null=False)
     def __unicode__(self):
         return self.display_name
--- backdoor_central_web/views.py
+++ backdoor_central_web/views.py
@@ -12,13 +12,35 @@ import threading, Queue
 from django.contrib.auth.models import *
 import datetime
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 from django.contrib.auth.forms import PasswordResetForm
+
 
 def index(request):
     return render(request, 'index.html')
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'login.html', {"login_form": AuthenticationForm()})
+
 
 def authenticate(request):
-    return render(request, 'login.html')
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        user = auth.authenticate(username=request.POST.get('username', ''), password=request.POST.get('password', ''))
+        if user is not None:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, 'login.html', {"login_form": AuthenticationForm(), "errors": "User %s does not exist"%(request.POST.get('username', ''))})
+
+# def login_authenticate(request):
+#     if request.user.is_authenticated():
+#         return HttpResponseRedirect(reverse("index"))
+#     else:
+#         user = auth.authenticate(username=request.POST.get('username', ''), password=request.POST.get('password', ''))
+#         if user is not None:
+#             auth.login(request, user)
+#             return HttpResponseRedirect(reverse("index"))
+#         else:
+#             return render(request, 'login.html', {"login_form": AuthenticationForm(), "errors": "User %s does not exist"%(request.POST.get('username', ''))})
